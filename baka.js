96603bffaa2cb31eb2a5365514d73f6a103aac4d
Samuel Culley <culleys1@secondary.ad.wagga.catholic.edu.au>
2021-04-26 14:27:37 +1000

    Added sound.

--- baka.js
+++ baka.js
@@ -2,4 +2,15 @@ function sus() {
     window.open("index.html")
 }
 
+var source = "amonguss.m4a"
+var audio = document.createElement("audio");
+//
+audio.autoplay = true;
+//
+audio.load()
+audio.addEventListener("load", function() { 
+    audio.play(); 
+}, true);
+audio.src = source;
+
 sus();
